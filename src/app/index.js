5ff078bafdf3659dd0998f2d97d8ecb48602b877
MrRoboman <rkayson@gmail.com>
2017-07-21 15:33:57 -0700

    small refactor cleaning up, organizing

--- src/app/index.js
+++ src/app/index.js
@@ -28,12 +28,12 @@ class App extends React.Component {
 
         this.interval = null;
         this.gameover = false;
-        this.difficulty = "beginner"; // grab this from localStorage
+        this.difficulty = "test"; // grab this from localStorage
         this.boardSettings = {
             test: {
                 rows: 1,
                 columns: 16,
-                totalBombs: 7
+                totalBombs: 1
             },
             beginner: {
                 rows: 8,
@@ -47,117 +47,33 @@ class App extends React.Component {
         // const tiles = this.getTestTiles();
         this.iterateAdjacentBombCounts(tiles);
 
+        const { totalBombs } = this.getBoardSetup();
+
         this.state = {
-            bombsRemaining: this.getBoardSetup().totalBombs,
+            bombsRemaining: totalBombs,
             secondsElapsed: 0,
             faceFrame: this.faceFrames.SMILE,
             tiles
         };
     }
 
-    onLeftClick(index) {
-        console.log("left click" + index);
-        if (this.gameover) {
-            return;
-        }
-        this.startTimer();
-        const tiles = this.revealTiles(index, this.state.tiles.slice());
-        let { bombsRemaining } = this.state;
-        let faceFrame = this.faceFrames.SMILE;
-        if (this.checkForWin(tiles)) {
-            console.log("winner");
-            this.gameover = true;
-            this.stopTimer();
-            this.flagRemainingBombs(tiles);
-            bombsRemaining = 0;
-            faceFrame = this.faceFrames.SHADES;
-        }
-        this.setState({
-            bombsRemaining,
-            faceFrame,
-            tiles
-        });
-    }
-
-    onRightClick(index) {
-        console.log("right click" + index);
-        if (this.gameover) {
-            return;
-        }
-        console.log(this.state)
-        const tiles = this.markTile(index, this.state.tiles.slice());
-
-        let { bombsRemaining } = this.state;
-        if (tiles[index].tileFrame === this.tileFrames.FLAGGED) {
-            bombsRemaining--;
-        } else {
-            bombsRemaining++;
-        }
-
-        this.setState({
-            bombsRemaining,
-            tiles
-        });
-    }
-
-    updateTimer() {
-        console.log('updatetimer');
-        this.setState({
-            secondsElapsed: this.state.secondsElapsed + 1
-        });
-    }
-
-    startTimer() {
-        console.log('starttimer');
-        if (!this.interval) {
-            this.interval = window.setInterval(this.updateTimer.bind(this), 1000);
-        }
-    }
-
-    stopTimer() {
-        if (this.interval) {
-            window.clearInterval(this.interval);
-            this.interval = null;
-        }
-    }
-
-    checkForWin(tiles) {
-        for(let i = 0; i < tiles.length; i++) {
-            const tile = tiles[i];
-            if(tile.tileFrame !== this.tileFrames.REVEALED && !tile.isBomb) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    flagRemainingBombs(tiles) {
-        for(let i = 0; i < tiles.length; i++) {
-            const tile = tiles[i];
-            if(tile.isBomb) {
-                tile.tileFrame = this.tileFrames.FLAGGED;
-            }
-        }
-    }
-
-    markTile(index, tiles) {
-        const markFrames = [
-            this.tileFrames.HIDDEN,
-            this.tileFrames.FLAGGED
-        ];
-
-        const tile = tiles[index];
-        const currentMark = markFrames.indexOf(tile.tileFrame);
-        if (currentMark > -1) {
-            const nextMark = markFrames[(currentMark + 1) % markFrames.length];
-            tile.tileFrame = nextMark;
-        }
-
-        return tiles;
-    }
-
     getBoardSetup() {
         return this.boardSettings[this.difficulty];
+    }
+
+    reset() {
+        this.gameover = false;
+        this.stopTimer();
+
+        const tiles = this.getNewShuffledTiles();
+        this.iterateAdjacentBombCounts(tiles);
+
+        this.setState({
+            bombsRemaining: this.getBoardSetup().totalBombs,
+            secondsElapsed: 0,
+            faceFrame: this.faceFrames.SMILE,
+            tiles
+        });
     }
 
     shuffle(tiles) {
@@ -198,6 +114,106 @@ class App extends React.Component {
         return tiles;
     }
 
+    onLeftClick(index) {
+        if (this.gameover) {
+            return;
+        }
+
+        this.startTimer();
+
+        const tiles = this.revealTiles(index, this.state.tiles.slice());
+        let { bombsRemaining } = this.state;
+        let faceFrame = this.faceFrames.SMILE;
+
+        if (this.checkForWin(tiles)) {
+            this.gameover = true;
+            this.stopTimer();
+            this.flagAllBombs(tiles);
+            bombsRemaining = 0;
+            faceFrame = this.faceFrames.SHADES;
+        }
+
+        this.setState({
+            bombsRemaining,
+            faceFrame,
+            tiles
+        });
+    }
+
+    onRightClick(index) {
+        if (this.gameover) {
+            return;
+        }
+
+        const tiles = this.markTile(index, this.state.tiles.slice());
+
+        let { bombsRemaining } = this.state;
+        if (tiles[index].tileFrame === this.tileFrames.HIDDEN) {
+            bombsRemaining++;
+        } else {
+            bombsRemaining--;
+        }
+
+        this.setState({
+            bombsRemaining,
+            tiles
+        });
+    }
+
+    startTimer() {
+        if (!this.interval) {
+            this.interval = window.setInterval(this.updateTimer.bind(this), 1000);
+        }
+    }
+
+    updateTimer() {
+        this.setState({
+            secondsElapsed: this.state.secondsElapsed + 1
+        });
+    }
+
+    stopTimer() {
+        if (this.interval) {
+            window.clearInterval(this.interval);
+            this.interval = null;
+        }
+    }
+
+    checkForWin(tiles) {
+        for(let i = 0; i < tiles.length; i++) {
+            const tile = tiles[i];
+            if(tile.tileFrame !== this.tileFrames.REVEALED && !tile.isBomb) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    flagAllBombs(tiles) {
+        tiles.forEach(tile => {
+            if (tile.isBomb) {
+                tile.tileFrame = this.tileFrames.FLAGGED;
+            }
+        });
+    }
+
+    markTile(index, tiles) {
+        const markFrames = [
+            this.tileFrames.HIDDEN,
+            this.tileFrames.FLAGGED,
+            this.tileFrames.QUESTION_MARK
+        ];
+
+        const tile = tiles[index];
+        const currentMark = markFrames.indexOf(tile.tileFrame);
+        if (currentMark > -1) {
+            const nextMark = markFrames[(currentMark + 1) % markFrames.length];
+            tile.tileFrame = nextMark;
+        }
+
+        return tiles;
+    }
+
     getAdjacentTileIndices(index) {
         const { rows, columns } = this.getBoardSetup();
         const totalTiles = rows * columns;
@@ -224,21 +240,6 @@ class App extends React.Component {
         });
 
         return adjacentIndices;
-        // const adjacentTiles = [];
-        // deltaIndices.forEach(delta => {
-        //     const adjacentIndex = index + delta;
-        //     if (adjacentIndex >= 0 && adjacentIndex < tiles.length) {
-        //         const thisColumn = index % columns;
-        //         const otherColumn = adjacentIndex % columns;
-        //         const columnsAreAdjacent = Math.abs(thisColumn - otherColumn) <= 1;
-        //
-        //         if (columnsAreAdjacent) {
-        //             adjacentTiles.push(tiles[adjacentIndex]);
-        //         }
-        //     }
-        // });
-        //
-        // return adjacentTiles;
     }
 
     iterateAdjacentBombCounts(tiles) {
@@ -284,21 +285,6 @@ class App extends React.Component {
         });
 
         return tiles;
-    }
-
-    reset() {
-        this.gameover = false;
-        this.stopTimer();
-
-        const tiles = this.getNewShuffledTiles();
-        this.iterateAdjacentBombCounts(tiles);
-
-        this.setState({
-            bombsRemaining: this.getBoardSetup().totalBombs,
-            secondsElapsed: 0,
-            faceFrame: this.faceFrames.SMILE,
-            tiles
-        });
     }
 
     render() {
