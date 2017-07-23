c28c4c61156eaef8c2af04adb3b91949602d4b97
MrRoboman <rkayson@gmail.com>
2017-07-23 11:56:11 -0700

    added minus digit

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,14 +4,18 @@ import Digit from './digit';
 
 export default function Counter(props) {
     let { value } = props;
-    if (value < 0) {
-        value = 0;
-    } else if (value > 999) {
+    if (value > 999) {
         value = 999;
     }
-    const hundreds = Math.floor(value / 100) % 10;
-    const tens = Math.floor(value / 10) % 10;
-    const ones = value % 10;
+    
+    let hundreds, tens, ones;
+    hundreds = Math.floor(Math.abs(value) / 100) % 10;
+    tens = Math.floor(Math.abs(value) / 10) % 10;
+    ones = Math.abs(value) % 10;
+
+    if (value < 0) {
+        hundreds = "minus";
+    }
 
     return (
             <div className="counter">
